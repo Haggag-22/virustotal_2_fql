33fc62fca228f3e5ff5ea1b88be782a0121ebfba
Haggag-22 <omhg22@gmail.com>
2025-04-22 07:43:59 -0400

    Updated

--- backend/setup.py
+++ backend/setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="vt2fql",
     version="1.0.0",
+    description="Generate CrowdStrike Falcon queries from VirusTotal IOCs",
+    author="Omar Haggag",
     packages=find_packages(),
     install_requires=[
         "click",
@@ -10,15 +12,9 @@ setup(
         "python-dotenv"
     ],
     entry_points={
-        'console_scripts': [
-            'vt2fql=vt2fql.cli:main',
-        ],
+        "console_scripts": [
+            "vt2fql=vt2fql.cli:main"
+        ]
     },
-    author="Omar Haggag",
-    description="Generate FQL queries from VirusTotal IOCs",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
     python_requires=">=3.8",
 )
